eb4c86613624315f5939f1bde29ee1394a9b80a2
Carlos Castrillo <cjcastrillo@cpp.edu>
2021-09-02 16:57:19 -0700

    implements the BagInerface with temp methods

--- bag.java
+++ bag.java
@@ -1,4 +1,4 @@
-public class bag {
+public class bag implements BagInterface{
     public boolean add(T newEntry){
         /*test if newEntry is a valid object*/
         /*test if bag is full*/
@@ -22,5 +22,47 @@ public class bag {
             }
             return items;
         }
+    }
+
+    @Override
+    public int getCurrentSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Object remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object anEntry) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getFrequencyOf(Object anEntry) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object anEntry) {
+        // TODO Auto-generated method stub
+        return false;
     }
 }
