2946efa0717414bfa375c4028c4bda7035678a6c
Carlos Castrillo <cjcastrillo@cpp.edu>
2021-09-23 17:05:39 -0700

    Added empty test methods with code to run them in main

--- ArrayBagTest.java
+++ ArrayBagTest.java
@@ -1,5 +1,23 @@
-public class ArrayBagTest{
+public class ArrayBagTest {
     public static void main(String[] args) {
-        
+        String[] contents1 = {"one","two","three"};
+        String[] contents2= {"one","two","three","four","five"};
+        ResizableArrayBag<String> bag1 = new ResizableArrayBag<String>(contents1);
+        ResizableArrayBag<String> bag2 = new ResizableArrayBag<String>(contents2);
+        testUnion(bag1,bag2);
+        testIntersection(bag1,bag2);
+        testDifference(bag1,bag2); 
+    }
+
+    private static void testUnion(Object bag1, Object bag2){
+
+    }
+    
+    private static void testIntersection(Object bag1, Object bag2){
+
+    }
+    
+    private static void testDifference(Object bag1, Object bag2){
+
     }
 }
