c9b0daccea709973b76dc99444a918788bd9e79c
Carlos Castrillo <cjcastrillo@cpp.edu>
2021-09-24 23:14:27 -0700

    Updated file to source code variant

--- LinkedBag.java
+++ LinkedBag.java
@@ -1,123 +1,145 @@
-public class LinkedBag<T> implements BagInterface<T> {
-    private Node firstNode;
-    private int numberOfEntries;
+/**
+    A class of bags whose entries are stored in a chain of linked nodes.
+    The bag is never full.
+    INCOMPLETE DEFINITION; includes definitions for the methods add,
+    toArray, isEmpty, and getCurrentSize.
+    @author Frank M. Carrano, Timothy M. Henry
+    @version 5.0
+*/
+public final class LinkedBag<T> implements BagInterface<T>
+{
+	private Node firstNode;       // Reference to first node
+	private int numberOfEntries;
 
-    public boolean add(T newEntry){
-        /*test if newEntry is a valid object*/
-        /*test if bag is full*/
-        Node newNode = new Node(newEntry);
-        newNode.net = firstNode;
-        firstNode = newNode;
-        numberOfEntries++;  
-        return true;
-    }
+	public LinkedBag()
+	{
+		firstNode = null;
+      numberOfEntries = 0;
+	} // end default constructor
 
-    @Override
-    public int getCurrentSize() {
-        // TODO Auto-generated method stub
-        return numberOfEntries;
-    }
+	/** Adds a new entry to this bag.
+	    @param newEntry  The object to be added as a new entry.
+	    @return  True. */
+	public boolean add(T newEntry) // OutOfMemoryError possible
+	{
+      // Add to beginning of chain:
+		Node newNode = new Node(newEntry);
+		newNode.next = firstNode;  // Make new node reference rest of chain
+                                 // (firstNode is null if chain is empty)
+      firstNode = newNode;       // New node is at beginning of chain
+		numberOfEntries++;
+      
+		return true;
+	} // end add
 
+	/** Retrieves all entries that are in this bag.
+       @return  A newly allocated array of all the entries in this bag. */
+	public T[] toArray()
+	{
+      // The cast is safe because the new array contains null entries.
+      @SuppressWarnings("unchecked")
+      T[] result = (T[])new Object[numberOfEntries]; // Unchecked cast
+      
+      int index = 0;
+      Node currentNode = firstNode;
+      while ((index < numberOfEntries) && (currentNode != null))
+      {
+         result[index] = currentNode.data;
+         index++;
+         currentNode = currentNode.next;
+      } // end while
+      
+      return result;
+      // Note: The body of this method could consist of one return statement,
+      // if you call Arrays.copyOf
+	} // end toArray
+   
+	/** Sees whether this bag is empty.
+       @return  True if the bag is empty, or false if not. */
+	public boolean isEmpty()
+	{
+		return numberOfEntries == 0;
+	} // end isEmpty
+   
+	/** Gets the number of entries currently in this bag.
+       @return  The integer number of entries currently in the bag. */
+	public int getCurrentSize()
+	{
+		return numberOfEntries;
+	} // end getCurrentSize
+   
+// STUBS:
 
-    @Override
-    public boolean isEmpty() {
-        // TODO Auto-generated method stub
-            return numberOfEntries == 0;
-            
-    }
+	/** Removes one unspecified entry from this bag, if possible.
+       @return  Either the removed entry, if the removal
+                was successful, or null. */
+	public T remove()
+   {
+      return null; // STUB
+   } // end remove
+   
+	/** Removes one occurrence of a given entry from this bag.
+       @param anEntry  The entry to be removed.
+       @return  True if the removal was successful, or false otherwise. */
+   public boolean remove(T anEntry)
+   {
+      return false; // STUB
+   } // end remove
+	
+	/** Removes all entries from this bag. */
+	public void clear()
+   {
+      // STUB
+   } // end clear
+	
+	/** Counts the number of times a given entry appears in this bag.
+		 @param anEntry  The entry to be counted.
+		 @return  The number of times anEntry appears in the bag. */
+	public int getFrequencyOf(T anEntry)
+   {
+      return 0; // STUB
+   } // end getFrequencyOf
+	
+	/** Tests whether this bag contains a given entry.
+		 @param anEntry  The entry to locate.
+		 @return  True if the bag contains anEntry, or false otherwise. */
+	public boolean contains(T anEntry)
+   {
+      return false; // STUB
+   } // end contains
 
-    @Override
-    public T remove() {
-        // TODO Auto-generated method stub
-        T result = null;
-        if (firstNode !=null)
-        {
-            result = firstNode.getData();
-            firstNode = firstNode.getNextNode();
-            numberOfEntries--;
-        }        
-        return result;
-    }
+	@Override
+	public Object union(Object bag1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public boolean remove(T anEntry) {
-        // TODO Auto-generated method stub
-        boolean result = false;
-        Node nodeN = getReferenceTo(anEntry);
+	@Override
+	public Object intersection(Object bag1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-        if (nodeN != null)
-        {
-            //Replace located entry
-            nodeN.setData(firstNode.getData());
-            //remove first node
-            firstNode = firstNode.getNextNode();
+	@Override
+	public Object difference(Object bag1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-            numberOfEntries--;
-            result = true;
-        }
-        return result;
-    }    
+	private class Node
+	{
+	  private T    data; // Entry in bag
+	  private Node next; // Link to next node
 
-    @Override
-    public void clear() {
-        // TODO Auto-generated method stub
-        while(!isEmpty())
-        remove();
-    }
-
-    @Override
-    public int getFrequencyOf(T anEntry) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public boolean contains(T anEntry) {
-        // TODO Auto-generated method stub
-        boolean contain = false;
-        for (int i = 0; i = T.length(); i++) {
-            {
-                if (T[i] = this.anEntry)
-                {
-                    contain = true;
-                }
-            }        
-        }
-        return contain;
-    }
-
- public class Node
- {
-
-    private T data;
-    private Node next;
-    private Node(T anEntry) {
-       this(anEntry, null);
-    }
-
-    private Node(T anEntry, Node nextNode) {
-        data = anEntry;
-        next = nextNode; 
-    }
-
-    private T getData()
-    {
-        return data;
-    }
-
-    private void setData(T newData)
-    {
-        data = newData;
-    }
-
-    private T getNextNode()
-    {
-        return next;
-    }
-
-    private void setNextNode(Node nextNode)
-    {
-        next = nextNode;
-    }
- }
-}
+		private Node(T dataPortion)
+		{
+			this(dataPortion, null);	
+		} // end constructor
+		
+		private Node(T dataPortion, Node nextNode)
+		{
+			data = dataPortion;
+			next = nextNode;	
+		} // end constructor
+	} // end Node
+} // end LinkedBag
